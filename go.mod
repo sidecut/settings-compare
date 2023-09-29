1378331f414bcd9db0dcc938be43ca8423660de2
Jim Raden <james.raden@gmail.com>
2023-09-29 11:56:32 -0400

    Import go code from another project

--- go.mod
+++ go.mod
@@ -1,3 +1,15 @@
 module settings-compare
 
 go 1.21
+
+require (
+	github.com/spf13/pflag v1.0.5
+	github.com/stretchr/testify v1.8.4
+	golang.org/x/exp v0.0.0-20230905200255-921286631fa9
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ main_test.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNormalizeToJson_Simple(t *testing.T) {
+	kvs := []KeyValue{{"a", "b"}, {"b", "c"}}
+	m, err := mapFromKeyValues(kvs, nil)
+
+	assert.Nil(t, err)
+
+	assert.Equal(t, 2, len(m), "Should have 2 keys")
+	assert.Equal(t, m["a"], "b")
+	assert.Equal(t, m["b"], "c")
+}
+
+func TestNormalizeToJson_1Key(t *testing.T) {
+	kvs := []KeyValue{{"a:a", "b"}, {"a:b", "c"}}
+	m, err := mapFromKeyValues(kvs, nil)
+
+	assert.Nil(t, err)
+
+	assert.Equal(t, 1, len(m), "Should have 1 key")
+}
+
+func TestNormalizeToJson_2Keys(t *testing.T) {
+	kvs := []KeyValue{{"a:a", "b"}, {"a:b", "c"}, {"b:a", "bb"}, {"b:b", "cc"}}
+	m, err := mapFromKeyValues(kvs, nil)
+
+	assert.Nil(t, err)
+
+	assert.Equal(t, 2, len(m), "Should have 2 keys")
+}
+
+func TestNormalizeToJson_3Keys_1notNested(t *testing.T) {
+	kvs := []KeyValue{{"a:a", "b"}, {"1", "one"}, {"a:b", "c"}, {"b:a", "bb"}, {"b:b", "cc"}}
+	m, err := mapFromKeyValues(kvs, nil)
+
+	assert.Nil(t, err)
+
+	assert.Equal(t, 3, len(m), "Should have 3 keys")
+
+	kvs2 := getKeyValues(m, "")
+	assert.Equal(t, 5, len(kvs2))
+}
+
+func TestNormalizeToJson_0Keys(t *testing.T) {
+	kvs := []KeyValue{}
+	m, err := mapFromKeyValues(kvs, nil)
+
+	assert.Nil(t, err)
+
+	assert.Equal(t, 0, len(m), "Should have 0 keys")
+}
